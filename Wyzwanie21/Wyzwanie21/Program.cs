72d6a54876dd3aa4921ae386ac642616d53da230
Kuba Brzeski <soryaleni@gmail.com>
2023-02-26 21:52:20 +0100

    Dzien 5

--- Wyzwanie21/Wyzwanie21/Program.cs
+++ Wyzwanie21/Wyzwanie21/Program.cs
@@ -1,20 +1,59 @@
-﻿
-var name = "Kuba";
-char sex = 'm';
-int age = 26;
+﻿/*
+//Tablice
+int[] points= new int[5];
+string[] daysOfWeeks= new string[7];
 
-if (age <= 30 && sex == 'm')
+daysOfWeeks[0] = "poniedzialek";
+daysOfWeeks[1] = "wtorek";
+daysOfWeeks[2] = "sroda";
+daysOfWeeks[3] = "czwartek";
+daysOfWeeks[4] = "piatek";
+daysOfWeeks[5] = "sobota";
+daysOfWeeks[6] = "niedziela";
+
+//inny sposob na tablice
+string[] daysOfWeeks2= {"pon","wt"};
+Console.WriteLine(daysOfWeeks2[1]);
+
+for(int i = 0; i < daysOfWeeks.Length; i++)
 {
-    Console.WriteLine("Mezczyzna poniezj 30 lat");
+    Console.WriteLine(daysOfWeeks[i]);
+}
+Console.WriteLine(" ");
+foreach(var day in daysOfWeeks)
+{
+    Console.WriteLine(day);
 }
 
-Console.WriteLine($"{name}, lat {age}");
+//Listy (tablice na sterydach)
+List<string> daysOfWeek3= new List<string>();
+foreach(string day in daysOfWeeks)
+{
+    daysOfWeek3.Add(day);
+} 
 
-if(age > 18)
+for(int i = 0;i < daysOfWeek3.Count; i++)
 {
-    Console.WriteLine("Pelnoletni mezczyzna");
+    Console.WriteLine(daysOfWeek3[i]);
+}*/
+
+int number = 1564161;
+string numberInString= number.ToString();
+char[] letters = numberInString.ToArray();
+
+char[] numbersToCheck = { '0', '1', '2', '3','4','5','6','7','8','9' };
+int[] result= {0,0,0,0,0,0,0,0,0,0};
+
+for(int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < letters.Length; j++) 
+        if (numbersToCheck[i] == letters[j])
+        {
+            result[i]++;
+        }
 }
-else
+
+for (int i = 0;i<10; i++)
 {
-    Console.WriteLine("Niepelnoletni mezczyzna");
+    Console.WriteLine(i+" => " + result[i]);
 }
