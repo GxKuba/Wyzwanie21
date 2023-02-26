366146d01e00eca9fbb48836a27ed2051e7a09d4
Kuba Brzeski <soryaleni@gmail.com>
2023-02-26 20:23:16 +0100

    Dzien 4

--- Wyzwanie21/Wyzwanie21/Program.cs
+++ Wyzwanie21/Wyzwanie21/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var name = "Kuba";
+char sex = 'm';
+int age = 26;
+
+if (age <= 30 && sex == 'm')
+{
+    Console.WriteLine("Mezczyzna poniezj 30 lat");
+}
+
+Console.WriteLine($"{name}, lat {age}");
+
+if(age > 18)
+{
+    Console.WriteLine("Pelnoletni mezczyzna");
+}
+else
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
